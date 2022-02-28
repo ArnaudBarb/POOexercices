a9b84db26ff28574efd174923623d99386ff2085
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-28 16:40:45 +0100

    appel de la classe User insertion de données users

--- index.php
+++ index.php
@@ -10,4 +10,14 @@ spl_autoload_register(function($className)
   include './classes/' . $className . '.php';
 });
 
+$michel = new User();
 
+$michel->setNomUser('Neimar');
+$michel->setFirstnameUser('Jean');
+$michel->setMailUser('raslebol@gmail.com');
+$michel->setDateOfBirth('1973-02-28');
+
+//on doit instancier l'objet avec new Sql sinon les parametres sont considérés comme vides
+$connexion = new Sql();
+
+$michel->subscribe($connexion);
