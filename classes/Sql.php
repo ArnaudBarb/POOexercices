5175ba493c877d94ad227bde1f880e7b9f27653d
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-28 12:42:57 +0100

    classe sql avec encapsulation, construct et destruct

---
+++ classes/Sql.php
@@ -0,0 +1,34 @@
+<?php
+
+class Sql
+{
+//encapsulation = définir si une classe est public, protected ou private
+
+    private string $serverName = "localhost";
+    private string $userName = "root";
+    private string $database = "poo";
+    private string $userPassword = "";
+    private object $connexion;
+
+    public function __construct()
+    {
+        try
+        {
+            $this->connexion = new PDO("mysql:host=$this->serverName;
+            dbname=$this->database", $this->userName, $this->userPassword);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        //PDOexception est une classe et le $e est l'instanciation d'objet = injection de dépendance
+        //l'importance des injections de dépendance tient dans l'absence de couplage (aka de dépendance 
+        //de classes. C'est pour cette raison que l'on sépare le css de l'html par exemple)
+        catch(PDOException $e)
+        {
+            die("Erreur :" . $e->getMessage);
+        }
+    }
+    public function __destruct()
+    {
+        unset($this->connexion);
+    }
+}
