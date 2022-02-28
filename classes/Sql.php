045adbd03630f6b0a9cc7bb92ebb5d115464fc47
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-28 16:42:00 +0100

    insertion de la fonction insertion()

--- classes/Sql.php
+++ classes/Sql.php
@@ -19,14 +19,30 @@ class Sql
             $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
 
-        //PDOexception est une classe et le $e est l'instanciation d'objet = injection de dépendance
-        //l'importance des injections de dépendance tient dans l'absence de couplage (aka de dépendance 
-        //de classes. C'est pour cette raison que l'on sépare le css de l'html par exemple)
         catch(PDOException $e)
         {
             die("Erreur :" . $e->getMessage);
         }
+        //PDOexception est une classe et le $e est l'instanciation d'objet = injection de dépendance
+        //l'importance des injections de dépendance tient dans l'absence de couplage (aka de dépendance 
+        //de classes. C'est pour cette raison que l'on sépare le css de l'html par exemple)
     }
+
+    public function insertion($requete)
+    {
+        try
+        {
+            $this->connexion->beginTransaction();
+            $this->connexion->exec($requete);
+            $this->connexion->commit();
+        }
+        catch(PDOException $e)
+        {
+            $this->connexion->rollback();
+            die("Erreur :" . $e->getMessage);
+        }
+    }
+
     public function __destruct()
     {
         unset($this->connexion);
